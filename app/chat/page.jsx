5f0b093e58438413c8c1de6a90d997b6a8fdd749
bae080311 <s24024@gsm.hs.kr>
2024-12-22 02:31:19 +0900

    Fix: error

---
+++ app/chat/page.jsx
@@ -0,0 +1,7 @@
+"use client";
+
+import styles from "./styles";
+
+export default function ChatPage() {
+  return <styles.Container></styles.Container>;
+}
---
+++ app/chat/styles.js
@@ -0,0 +1,11 @@
+const { default: styled } = require("styled-components");
+
+const Container = styled.div``;
+
+const input = styled.input``;
+
+const inputContainer = styled.div``;
+
+const topBar = styled.div``;
+
+export default { Container, input, inputContainer, topBar };
