b7d196b203fce5886283c7a7651bb90b46825768
bae080311 <s24024@gsm.hs.kr>
2024-12-21 20:56:21 +0900

    Feat: button

--- app/components/button/button.jsx
+++ app/components/button/button.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "./styles";
 
-export default function Button({ label }) {
+export default function Button(label) {
   return <Button>{label}</Button>;
 }
