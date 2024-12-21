ae5b8aeac4aa427a08816c0ce2833bd6a0b4130f
bae080311 <s24024@gsm.hs.kr>
2024-12-21 22:03:38 +0900

    Fix: header style

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -1,11 +1,13 @@
 "use client";
 
 import styles from "./styles";
+import Text from "../../svgs/text.svg";
+import Image from "next/image";
 
 export function Header() {
   return (
     <styles.Container>
-      <h1>BOBMUKZA</h1>
+      <Image style={{ zIndex: 1000 }} src={Text} alt="text" />
     </styles.Container>
   );
 }
--- app/components/header/styles.js
+++ app/components/header/styles.js
@@ -4,11 +4,10 @@ const Container = styled.header`
   display: flex;
   justify-content: center;
   align-items: end;
-  bottom-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
-  background-color: #d8cd93;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1); /* box-shadow로 수정 */
+  background-color: rgba(216, 205, 147, 0.19); /* 19% 투명도 */
   padding: 1rem;
   padding-top: 2rem;
-  opacity: 19%;
 `;
 
 export default { Container };
