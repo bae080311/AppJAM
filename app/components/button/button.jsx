2b6c0aaf787a47936d7480da53c5ca5c5e09a91d
bae080311 <s24024@gsm.hs.kr>
2024-12-21 19:19:32 +0900

    Feat: button

---
+++ app/components/button/button.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Button from "./styles";
+
+export default function Button({ label }) {
+  return <Button>{label}</Button>;
+}
---
+++ app/components/button/styles.js
@@ -0,0 +1,6 @@
+export const Button = styled.button`
+  border: 1px solid #000;
+  border-radius: 5px;
+  padding: 10px 20px;
+`;
+//className="w-[400px] mt-6 py-3 border text-black1 border-black1 rounded"
