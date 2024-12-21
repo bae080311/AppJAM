171dd31ffb874531a1ffae90a154b81f0d299b05
bae080311 <s24024@gsm.hs.kr>
2024-12-21 21:50:00 +0900

    Feat: data format

--- app/components/picker.jsx
+++ app/components/picker.jsx
@@ -1,15 +1,34 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import dayjs from "dayjs";
+import WritePage from "../write/page";
 
 export default function BasicDateTimePicker() {
+  const [value, setValue] = useState(dayjs("2024-12-10T12:43:08.798Z"));
+
+  const formatDate = (date) => dayjs(date).format("YYYY-MM-DD HH:mm:ss");
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+
+    const formattedDate = formatDate(newValue);
+    <WritePage date={formattedDate} />;
+  };
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DemoContainer components={["DateTimePicker"]}>
-        <DateTimePicker sx={{ width: 300 }} label="날짜를 선택해주세요" />
+        <DateTimePicker
+          sx={{ width: 300 }}
+          label="날짜를 선택해주세요"
+          value={value}
+          onChange={handleChange}
+        />
       </DemoContainer>
+      <div>현재 값: {formatDate(value)}</div>
     </LocalizationProvider>
   );
 }
