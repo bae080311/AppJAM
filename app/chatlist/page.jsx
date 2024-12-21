ee5873e4095b65f17279f80de7fc9c3777dbe5d7
bae080311 <s24024@gsm.hs.kr>
2024-12-22 00:59:01 +0900

    Fix: 컴포넌트 분리리

--- app/chatlist/page.jsx
+++ app/chatlist/page.jsx
@@ -1,25 +1,11 @@
 "use client";
 
 import { Container, TopBar, ChatContainer, Name } from "./styles";
-import profile from "../svgs/bigprofile.svg";
-import Image from "next/image";
 
 export default function ChatList() {
   return (
     <Container>
       <TopBar>채팅</TopBar>
-      <ChatContainer>
-        <Image src={profile} alt="profile" />
-        <Name>배경진</Name>
-      </ChatContainer>
-      <ChatContainer>
-        <Image src={profile} alt="profile" />
-        <Name>배경진</Name>
-      </ChatContainer>
-      <ChatContainer>
-        <Image src={profile} alt="profile" />
-        <Name>배경진</Name>
-      </ChatContainer>
     </Container>
   );
 }
---
+++ app/components/chatlist/styles.js
@@ -0,0 +1,14 @@
+export const ChatContainer = styled.div`
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+export const Name = styled.span`
+  color: #000;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
